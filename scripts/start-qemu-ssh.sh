39da6e33b2f05a5b61cd0c27764520e62be867c5
SrijanRoy14 <routhroysrijan@gmail.com>
2026-04-26 15:18:52 +0000

    Updated the script and made it runnable

--- scripts/start-qemu-ssh.sh
+++ scripts/start-qemu-ssh.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
-
-BINARIES_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$BINARIES_DIR" || exit 1
-
-export PATH="/home/errorr/projects/emli/external/output/qemu_aarch64_br/host/bin:$PATH"
+set -e
+OUTPUT_DIR=~/projects/emli/external/output/qemu_aarch64_br
+ROOTFS_IMG="$OUTPUT_DIR/images/rootfs.ext4"
+KERNEL_IMG="$OUTPUT_DIR/images/Image"
 
 exec qemu-system-aarch64 \
   -M virt \
   -cpu cortex-a53 \
   -nographic \
   -smp 1 \
-  -kernel Image \
+  -kernel "$KERNEL_IMG" \
   -append "rootwait root=/dev/vda console=ttyAMA0" \
   -netdev user,id=net0,hostfwd=tcp::2222-:22 \
   -device virtio-net-device,netdev=net0 \
-  -drive file=rootfs.ext4,if=virtio,format=raw
+  -drive file="$ROOTFS_IMG",if=virtio,format=raw
